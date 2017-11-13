067d675f83f46c3fabefe7f52db74312a8e4d20f
JI005KU <JI005KU@IN-GFK27H2-10.pbi.global.pvt>
2017-11-13 23:17:19 +0530

    first version

---
+++ udf/src/main/java/jk/hive2/udf/App.java
@@ -0,0 +1,13 @@
+package jk.hive2.udf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
